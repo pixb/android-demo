3f7bf2b31fdfadfe231a818680139fb6697e6300
pix <tpxsky@163.com>
2020-01-03 17:53:17 +0800

    完成nexus module demo

--- TestNexusModule/app/src/main/java/com/pix/nexusmodule/MainActivity.kt
+++ TestNexusModule/app/src/main/java/com/pix/nexusmodule/MainActivity.kt
@@ -2,11 +2,14 @@ package com.pix.nexusmodule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pix.pixmodule.PixModule
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv_content.text = PixModule().getString()
     }
 }
