5487d747a63967bcb95b3d3f8c570907505cc0cb
pix <tpxsky@163.com>
2020-09-05 21:25:29 +0800

    add Fragment Demo

---
+++ test-fragment/hello-fragment/src/main/java/com/pix/hellofragment/HelloFragment.kt
@@ -0,0 +1,15 @@
+package com.pix.hellofragment
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Copyright (C), 2020-2020, pix
+ * Author: pix
+ * Date: 20-9-5
+ * Version: 1.0
+ * Description:
+ * History:
+ * <author> <time> <version> <desc>
+ */
+class HelloFragment : Fragment(R.layout.fragment_hello){
+}
---
+++ test-fragment/hello-fragment/src/main/java/com/pix/hellofragment/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.pix.hellofragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager.commit { add<HelloFragment>(R.id.fcv) }
+    }
+}
