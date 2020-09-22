832122950707b86663fff6d8b8e9870018090d60
pix <tpxsky@163.com>
2020-09-22 16:20:16 +0800

    升级androidx,支持kotlin

--- TestShape/app/src/main/java/com/pix/testshape/MainActivity.java
+++ TestShape/app/src/main/java/com/pix/testshape/MainActivity.java
@@ -1,14 +1,12 @@
 package com.pix.testshape;
 
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.tabs.TabLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
