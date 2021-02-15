85a2f79bd88acf2063331dd46d6f210b4dd1c79f
pix <tpxsky@163.com>
2021-02-15 17:15:10 +0800

    add use dialog create dialog fragment comment

--- TestDialogFragment/app/src/main/java/com/pix/testdialogfragment/CustomViewDialogFragment.kt
+++ TestDialogFragment/app/src/main/java/com/pix/testdialogfragment/CustomViewDialogFragment.kt
@@ -26,10 +26,19 @@ class CustomViewDialogFragment: DialogFragment() {
         // 设置主题的构造方法
 //        var builder = AlertDialog.Builder(activity, R.style.CustomDialog)
         var inflater = activity?.layoutInflater
-        var view = inflater?.inflate(R.layout.layout_custom_view_dialog, null);
+        var view = inflater?.inflate(R.layout.layout_custom_view_dialog, null)
         builder.setView(view)
         view?.findViewById<AppCompatButton>(R.id.btn_close)?.setOnClickListener { dismiss() }
         return builder.create()
+        // 另一种写法
+//        var inflater = activity?.layoutInflater
+//        var view = inflater?.inflate(R.layout.layout_custom_view_dialog, null)
+//        var dialog = Dialog(requireContext())
+//        // 设置主题的构造方法
+////    var dialog = Dialog(requireContext(), R.style.CustomDialog)
+//        dialog.setContentView(view);
+//        // Do Someting
+//        return dialog
     }
 
     override fun onAttachFragment(childFragment: Fragment) {
